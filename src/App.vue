3b8ad62e1ba04edced50b582a05a49a1af74c048
ZeroHourX <A17620949894@outlook.com>
2019-12-28 17:11:10 +0800

    引入字体图标和初始化CSS

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,101 @@
 <template>
-    <div id="app">
-        <router-link to="/login">登录页</router-link>
-
-        
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <!-- //占位组件 -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            message:"webpack"
-        }
-    },
-}
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
-    #app{
-        color: #f00;
-    }
+@import url(http://at.alicdn.com/t/font_1587733_m0evp29aygn.css);
+/*清除内外边距  */
+* {
+  margin: 0;
+  padding: 0;
+}
+/*让所有斜体 不倾斜*/
+em,
+i {
+  font-style: normal;
+}
+/*去掉列表前面的小点*/
+li {
+  list-style: none;
+}
+/*图片没有边框   去掉图片底侧的空白缝隙*/
+img {
+  border: 0; /*ie6*/
+  vertical-align: middle;
+}
+/*让button 按钮 变成小手*/
+button {
+  cursor: pointer;
+}
+/*取消链接的下划线*/
+a {
+  color: #666;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #e33333;
+}
+
+button,
+input {
+  font-family: Microsoft YaHei, Heiti SC, tahoma, arial, Hiragino Sans GB,
+    \\5b8b\4f53, sans-serif;
+}
+
+body {
+  -webkit-font-smoothing: antialiased;
+  background-color: #fff;
+  font: 12px/1.5 Microsoft YaHei, Heiti SC, tahoma, arial, Hiragino Sans GB,
+    \\5b8b\4f53, sans-serif;
+  color: #666;
+}
+
+.hide,
+.none {
+  display: none;
+}
+/*清除浮动*/
+.clearfix:after {
+  visibility: hidden;
+  clear: both;
+  display: block;
+  content: ".";
+  height: 0;
+}
+
+.clearfix {
+  *zoom: 1;
+}
+
+.fl {
+  float: left;
+}
+
+.fr {
+  float: right;
+}
+
+.al {
+  text-align: left;
+}
+
+.ac {
+  text-align: center;
+}
+
+.ar {
+  text-align: right;
+}
 </style>
--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-      <h1>Hello webpack & Vue</h1>
+    <div class="del">
+      <i class="iconfont icon-guanbi"></i>
+    </div>
+
+    <div class="logo">
+      <i class="iconfont icon-new"></i>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
