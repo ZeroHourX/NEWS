f91859bcd47037782640c939152f547cc25485ee
ZeroHourX <A17620949894@outlook.com>
2019-12-31 00:35:53 +0800

    首页tab栏和封装文章列表

---
+++ src/components/PostList.vue
@@ -0,0 +1,163 @@
+<template>
+  <div>
+    <!-- 一张图 -->
+    <div class="img_one" v-if="post.cover.length > 0 && post.cover.length < 3 && post.type===1">
+      <div class="title">
+        <p class="van-multi-ellipsis--l2">{{post.title}}</p>
+        <div class="user">
+          <p>{{post.user.nickname}}</p>
+          <p>{{post.comment_length}}跟帖</p>
+        </div>
+      </div>
+
+      <div class="img">
+        <img :src="post.cover[0].url" alt />
+      </div>
+    </div>
+
+    <!-- 三张图 -->
+    <div class="img_three" v-if="post.cover.length >= 3 && post.type===1">
+      <div class="title">
+        <p class="van-multi-ellipsis--l2">{{post.title}}</p>
+        <div class="img_if">
+          <div class="img" v-for="(item,index) in post.cover" :key="index">
+            <img v-if="index < 3" :src="item.url" alt />
+          </div>
+        </div>
+        <div class="user">
+          <p>{{post.user.nickname}}</p>
+          <p>{{post.comment_length}}跟帖</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- 视频 -->
+    <div class="video" v-if="post.type === 2">
+      <div class="title">
+        <p class="van-ellipsis">{{post.title}}</p>
+        <div class="img_if">
+          <div class="img">
+            <img :src="post.cover[0].url" alt />
+            <span class="video_tip"></span>
+          </div>
+        </div>
+        <div class="user">
+          <p>{{post.user.nickname}}</p>
+          <p>{{post.comment_length}}跟帖</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"]
+};
+</script>
+
+<style scoped lang="less">
+.img_one {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 10px;
+  border-bottom: 1px #ddd solid;
+  .title {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-size: 16px;
+    height: 75/360 * 100vw;
+    line-height: 1.3;
+  }
+  .img {
+    margin-left: 10px;
+    img {
+      width: 121/360 * 100vw;
+      height: 75/360 * 100vw;
+      object-fit: cover;
+    }
+  }
+}
+
+.img_three {
+  padding: 13px 10px;
+  border-bottom: 1px #ddd solid;
+  .title {
+    font-size: 16px;
+    .img_if {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px 0;
+      .img {
+        width: 33%;
+        height: 75/360 * 100vw;
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+    }
+  }
+}
+
+.video {
+  padding: 13px 10px;
+  border-bottom: 1px #ddd solid;
+  .title {
+    font-size: 16px;
+    .img_if {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px 0;
+      .img {
+        width: 100%;
+        height: 175/360 * 100vw;
+        position: relative;
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+        .video_tip {
+          width: 55/360 * 100vw;
+          height: 55/360 * 100vw;
+          border-radius: 50%;
+          background: rgba(0, 0, 0, 0.2);
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          &::before {
+            content: " ";
+            display: block;
+            overflow: hidden;
+            width: 0;
+            height: 0;
+            border-bottom: 0.9375rem solid transparent;
+            border-top: 0.9375rem solid transparent;
+            border-left: 1.25rem solid #fff;
+            border-radius: 5px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            margin: 0 3px;
+          }
+        }
+      }
+    }
+  }
+}
+
+.user {
+  display: flex;
+  p {
+    margin-right: 10px;
+    color: #bbb;
+    font-size: 14px;
+  }
+}
+</style>
--- src/page/ListIndex.vue
+++ src/page/ListIndex.vue
@@ -10,16 +10,66 @@
         <i class="iconfont icon-geren11"></i>
       </router-link>
     </div>
+
+    <van-tabs v-model="active" sticky animated swipeable background="#e4e4e4" @click="onClick">
+      <van-tab v-for="(item,index) in category" :title="item.name" :key="index">
+        <PostList v-for="(item,index) in list" :key="index" :post="item" />
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-export default {};
+import PostList from "@/components/PostList";
+export default {
+  data() {
+    return {
+      active: localStorage.getItem("token") ? 1 : 0,
+      category: [],
+      cid: 999,
+      list: []
+    };
+  },
+  methods: {
+    onClick(name, title) {
+      this.cid = this.category[this.active].id;
+      // 文章列表
+      this.$axios({
+        url: `/post?pageIndex=1&pageSize=10&category=${this.cid}`
+      }).then(res => {
+        this.list = res.data.data;
+      });
+    }
+  },
+  mounted() {
+    //栏目
+    const config = { url: "/category" };
+
+    if (localStorage.getItem("token")) {
+      config.headers = {
+        Authorization: localStorage.getItem("token")
+      };
+    }
+    this.$axios(config).then(res => {
+      this.category = res.data.data;
+    });
+
+    // 文章列表
+    this.$axios({
+      url: `/post?pageIndex=1&pageSize=10&category=${this.cid}`
+    }).then(res => {
+      this.list = res.data.data;
+    });
+  },
+  components: {
+    PostList
+  }
+};
 </script>
 
 <style scoped lang="less">
 .head_search {
-  height: 50/360 * 100vw;
+  height: 40/360 * 100vw;
   background: #ff0000;
   display: flex;
   justify-content: space-between;
@@ -32,11 +82,11 @@ export default {};
   .search {
     flex: 1;
     background: rgba(256, 256, 256, 0.5);
-    padding: 10px 20px;
+    padding: 5px 20px;
     margin: 0 20px;
     text-align: center;
     color: #fff;
-    font-size: 18px;
+    font-size: 14px;
     border-radius: 50px;
   }
   .geren i {
